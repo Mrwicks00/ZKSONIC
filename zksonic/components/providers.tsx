c3365e41f8b457e0841691165c8ac347cca8881a
Mrwicks00 <victoradeoba83@gmail.com>
2025-09-05 03:35:34 +0100

    still fixing........

--- zksonic/components/providers.tsx
+++ zksonic/components/providers.tsx
@@ -1,14 +1,17 @@
+// components/providers.tsx
 "use client";
 
-import React from "react";
+import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { config } from "@/lib/wagmi-client";
 
-const queryClient = new QueryClient();
+import { config } from "@/lib/wagmi";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+ 
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
